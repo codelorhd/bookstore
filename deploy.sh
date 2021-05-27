aa0a9f3faa1a77416bae7bb2ed49e98bfb59b2ad
Aboyeji, Solomon Adeleke <solesty7@gmail.com>
2021-05-27 17:47:04 +0100

    nginx+domain

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@
 # REMEMBER TO RUN THIS TO ACTIVATE THE BASH FILES
 # chmod +x deploy.sh
 
+# SSH root will work if the SSH of the pc as been activated in the cloud
+
 # remove the old files
 ssh root@104.236.57.23 'rm -r ~/bookstore'
 
@@ -16,6 +18,7 @@ scp -r Dockerfile  root@104.236.57.23:~/bookstore
 scp -r requirements.txt  root@104.236.57.23:~/bookstore
 scp -r ReadMe.md  root@104.236.57.23:~/bookstore
 scp -r .gitignore  root@104.236.57.23:~/bookstore
+scp -r nginx-reverse-proxy  root@104.236.57.23:~/bookstore
 
 # stop the docker running on the cloud
 ssh root@104.236.57.23 'docker stop bookstore-api'
@@ -26,3 +29,9 @@ ssh root@104.236.57.23 'docker build -t bookstore-api-build ~/bookstore'
 # you could use docker-compose here so you can keep 
 # PRODUCTION environment variables out of the repository
 ssh root@104.236.57.23 'docker run -idt --name=bookstore-api -e MODULE_NAME="run" -e PORT="3000" -e PRODUCTION="true" -p 3000:3000 bookstore-api-build'
+
+# build and create the nginx
+ssh root@104.236.57.23 'docker stop api-nginx'
+ssh root@104.236.57.23 'docker rm api-nginx'
+ssh root@104.236.57.23 'docker build -t bookstore-nginx ~/bookstore/nginx-reverse-proxy'
+ssh root@104.236.57.23 'docker run -idt --name=api-nginx -p 80:80 bookstore-nginx'
